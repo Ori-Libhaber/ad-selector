6784a9f33d7dabe56abd0313be1517445fb31ec6
ori.libhaber <ori.libhaber@gmail.com>
2023-05-29 22:26:35 +0300

    initial

---
+++ src/main/java/com/undertone/adselector/AdSelectorApplication.java
@@ -0,0 +1,13 @@
+package com.undertone.adselector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdSelectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdSelectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/undertone/adselector/AdSelectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.undertone.adselector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdSelectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
